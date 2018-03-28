13b68e5af930f52f179d6ce2d944b2d71f82cc80
omikron15 <omikron15@hotmail.co.uk>
2018-03-28 16:08:42 +0100

    Exercise A finished?

--- arrays_hashes_quiz.rb
+++ arrays_hashes_quiz.rb
@@ -10,17 +10,33 @@ stops = [ "Croy", "Cumbernauld", "Falkirk High", "Linlithgow", "Livingston", "Ha
 
 # 1. Add `"Edinburgh Waverley"` to the end of the array
 stops.push("Edinburgh Waverley")
-p stops
 # 2. Add `"Glasgow Queen St"` to the start of the array
+stops.unshift("Glasgow Queen St")
 # 3. Add `"Polmont"` at the appropriate point (between `"Falkirk High"` and `"Linlithgow"`)
+stops.insert(4,"Polmont")
 # 4. Work out the index position of `"Linlithgow"`
+stops.find_index("Linlithgow")
 # 5. Remove `"Livingston"` from the array using its name
+stops.delete("Linlithgow")
 # 6. Delete `"Cumbernauld"` from the array by index
+stops.delete_at(2)
 # 7. How many stops there are in the array?
+stops.count
 # 8. How many ways can we return `"Falkirk High"` from the array?
-# 9. Reverse the positions of the stops in the array
-# 10. Print out all the stops using a for loop
+stops[2]
+stops[-5]
+stops.fetch(2)
+stops.fetch(-5)
+stops.at(2)
+p stops.at(-5)
 
+# 9. Reverse the positions of the stops in the array
+reverse = stops.reverse
+reverse
+# 10. Print out all the stops using a for loop
+for each_stop in stops
+  p each_stop
+end
 ## Exercise B
 
 ### Given the following data structure:
